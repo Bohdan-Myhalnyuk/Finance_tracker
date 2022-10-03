81fa2125bffafa23d131e34f6c7e0970ca2236c9
Ubuntu <ubuntu@ip-172-31-83-74.ec2.internal>
2022-10-03 20:04:44 +0000

    Editing my_portfolio template

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,7 +1,16 @@
 class StocksController < ApplicationController
-	
-	def search
-		stock = Stock.new_lookup(params[:stock])
-		render json: stock
-	end
-end 
+  def search
+    if params[:stock].present?
+      @stock = Stock.new_lookup(params[:stock])
+      if @stock
+        render 'users/my_portfolio'
+      else
+        flash[:alert] = 'Please enter the valid symbol to search'
+        redirect_to my_portfolio_path
+      end
+    else
+      flash[:alert] = 'Please enter the symbol to search'
+      redirect_to my_portfolio_path
+    end
+  end
+end
