2e79e07a3326de76ae9dce140b9af773ef573c3d
Ubuntu <ubuntu@ip-172-31-83-74.ec2.internal>
2022-10-04 14:03:22 +0000

    Handling AJAX for my_porfolio

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -3,14 +3,20 @@ class StocksController < ApplicationController
     if params[:stock].present?
       @stock = Stock.new_lookup(params[:stock])
       if @stock
-        render 'users/my_portfolio'
+        respond_to do |format|
+          format.js { render partial: 'users/result' }
+        end
       else
-        flash[:alert] = 'Please enter the valid symbol to search'
-        redirect_to my_portfolio_path
+        respond_to do |format|
+          flash.now[:alert] = 'Please enter the valid symbol to search'
+          format.js { render partial: 'users/result' }
+        end
       end
     else
-      flash[:alert] = 'Please enter the symbol to search'
-      redirect_to my_portfolio_path
+      respond_to do |format|
+        flash.now[:alert] = 'Please enter the symbol to search'
+        format.js { render partial: 'users/result' }
+      end
     end
   end
 end
