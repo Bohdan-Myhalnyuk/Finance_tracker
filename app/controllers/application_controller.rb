f9d7ed05edd7bc356d414150c12255cae9a12930
Ubuntu <ubuntu@ip-172-31-83-74.ec2.internal>
2022-10-08 19:51:47 +0000

    Adding everything about friends

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :second_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[first_name second_name])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -22,9 +22,40 @@ class User < ApplicationRecord
   def can_track_stock?(ticker_symbol)
     under_stock_limit? && !stock_already_tracked?(ticker_symbol)
   end
-  
+
   def full_name
     return "#{first_name} #{second_name}" if first_name || second_name
-    "Anonymous"
-  end 
+    'Anonymous'
+  end
+  
+  def self.search(param)
+    param.strip!
+    to_send_back = (first_name_matches(param) + second_name_matches(param) + email_matches(param)).uniq
+    return nil unless to_send_back
+    to_send_back
+  end
+  
+  def self.first_name_matches(param)
+    matches('first_name', param)
+  end
+
+  def self.second_name_matches(param)
+    matches('second_name', param)
+  end
+
+  def self.email_matches(param)
+    matches('email', param)
+  end
+
+  def self.matches(field_name, param)
+    where("#{field_name} like ?", "%#{param}%")
+  end
+
+  def except_current_user(users)
+    users.reject { |user| user.id == self.id }
+  end
+
+  def not_friends_with?(id_of_friend)
+    !self.friends.where(id: id_of_friend).exists?
+  end
 end
